cf0f1b1464afb58c5f7cf93a8d7212d74ecea18c
aviralmansingka <aviralmansingka@gmail.com>
2022-06-12 15:13:34 -0700

    Adding box for Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen bg-gray-200">
+    <div className="h-screen bg-gray-200 px-4 py-4">
       <Head>
         <title>Batooney</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Home page</p>
+      <div className="flex justify-center bg-white px-4 py-4">
+        <h2>Home page</h2>
+      </div>
     </div>
   );
 };
